221bd3bfc04a204b608149bdfef8037edaf171cc
eedeenilson <eedeenilson@gmail.com>
2021-03-03 12:09:50 -0600

    Agrega apidoc

---
+++ apidoc/api/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "API Explorer para nodejs API-RESTFull",
+  "version": "0.1.0",
+  "description": "Guía ilustrada para usar nodejs API RESTful",
+  "title": "NodeJS RESTFul",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-03T17:47:14.596Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
